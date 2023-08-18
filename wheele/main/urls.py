8ec890d362bda26c2f5e989755e8e851e76e9cf9
Sanath <sanathbnaik@gmail.com>
2023-08-18 17:12:42 +0530

    first commit

---
+++ wheele/main/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+from django.contrib.auth import views as auth_views
+
+urlpatterns = [
+    path('login/', views.login_view, name='login'),
+    path('',views.landing_view,name = "home"),
+    path('dashboard/', views.dashboard_view, name='dashboard'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+]
