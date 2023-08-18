98ef560d1ce32d95e93d62beca27b294dee94ad2
Sanath <sanathbnaik@gmail.com>
2023-08-18 18:49:26 +0530

    images fixed

--- wheele/main/urls.py
+++ wheele/main/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
 from django.contrib.auth import views as auth_views
+from django.contrib.staticfiles.views import serve
+
 
 urlpatterns = [
     path('login/', views.login_view, name='login'),
