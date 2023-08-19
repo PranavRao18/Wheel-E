7c5f5c7fbe99e16697b62b1cceaecc528664d644
Sanath <sanathbnaik@gmail.com>
2023-08-19 17:04:26 +0530

    driver page

--- wheele/main/views.py
+++ wheele/main/views.py
@@ -7,24 +7,28 @@ from main.models import Offers
 from django.db.models import F, FloatField
 from math import radians,  sin, cos
 
+
 @login_required
 def dashboard_view(request):
-    
+
     user = User.objects.get(email=request.user)
     firstname = user.first_name
-    profile = {"first_name":firstname}
+    profile = {"first_name": firstname}
     if user.is_driver:
         print("Hello driver")
-        return render(request,'registration/driver-dashboard.html',{'profile':profile})
-    return render(request, 'registration/dashboard.html',{'profile':profile})
+        offers = Offers.objects.filter(is_active = True, is_accepted = False)
+        profile = [{'price': obj.final_price, 'distance': obj.distance} for obj in offers]
+        return render(request, 'registration/driver_dashboard.html', {'profile':{'first_name':firstname},'items': profile})
+    return render(request, 'registration/dashboard.html', {'profile': profile})
 
-    
 
 def landing_view(request):
     return render(request, 'home.html')
 
+
 def about_us(request):
-    return render(request,"about_us.html")
+    return render(request, "about_us.html")
+
 
 def handle_request(request):
     if request.method == 'POST':
@@ -45,32 +49,33 @@ def handle_request(request):
                 sedan = str((float(distance)-2)*15)
             if distance is not None:
                 # Save the distance to the database
-                Offers.objects.create(distance=distance,fromCoordinatesLat = latA,fromCoordinatesLong = lonA)
-                return JsonResponse({'message': 'Distance data saved successfully.',"data" :{"scooter":scooter,"auto":auto,"mini":mini,"sedan":sedan}})
-        
+                Offers.objects.create(
+                    distance=distance, fromCoordinatesLat=latA, fromCoordinatesLong=lonA)
+                return JsonResponse({'message': 'Distance data saved successfully.', "data": {"scooter": scooter, "auto": auto, "mini": mini, "sedan": sedan}})
+
         if data.get('type') == 'book':
-            offer = Offers.objects.get(requestId = data.get('requestId'))
+            offer = Offers.objects.get(requestId=data.get('requestId'))
             offer.is_active = True
             offer.user_id = data.get('user_id')
             offer.save()
             return JsonResponse({'message': 'Success.'})
-        
+
         if data.get('type') == 'fetch-driver':
-            offer = Offers.objects.get( requestId = data.get('requestId'))
+            offer = Offers.objects.get(requestId=data.get('requestId'))
             if offer.is_accepted:
                 driverid = offer.driver_id
-                driver = User.objects.get(id = driverid)
+                driver = User.objects.get(id=driverid)
                 message = {
                     'success': 'True',
-                    'name' : driver.first_name ,
-                    'phoneNo' : driver.phone_number,
-                    'license' : driver.license_number
+                    'name': driver.first_name,
+                    'phoneNo': driver.phone_number,
+                    'license': driver.license_number
                 }
             else:
                 message = {
-                    'success':'False'}
+                    'success': 'False'}
             return JsonResponse({'message': message})
-        
+
         if data.get('type') == 'get-offers':
             latA = float(data.get('latA'))
             lonA = float(data.get('lonA'))
@@ -94,12 +99,15 @@ def handle_request(request):
                 offers = Offers.objects.filter(
                     fromCoordinatesLat__isnull=False,
                     fromCoordinatesLong__isnull=False,
-                    lat_diff__range=(lat_diff - (0.5 * distance_threshold_km / earth_radius_km), lat_diff + (0.5 * distance_threshold_km / earth_radius_km)),
-                    lon_diff__range=(lon_diff - (0.5 * distance_threshold_km / earth_radius_km), lon_diff + (0.5 * distance_threshold_km / earth_radius_km)),
+                    lat_diff__range=(lat_diff - (0.5 * distance_threshold_km / earth_radius_km),
+                                     lat_diff + (0.5 * distance_threshold_km / earth_radius_km)),
+                    lon_diff__range=(lon_diff - (0.5 * distance_threshold_km / earth_radius_km),
+                                     lon_diff + (0.5 * distance_threshold_km / earth_radius_km)),
                     is_active=True,
                     is_accepted=False
                 ).annotate(
-                    distance_km=(sin(lat_diff / 2) ** 2 + cos(latA_rad) * cos(F('fromCoordinatesLat')) * sin(lon_diff / 2) ** 2) ** 0.5 * 2 * earth_radius_km,
+                    distance_km=(sin(lat_diff / 2) ** 2 + cos(latA_rad) * cos(
+                        F('fromCoordinatesLat')) * sin(lon_diff / 2) ** 2) ** 0.5 * 2 * earth_radius_km,
                     output_field=FloatField()
                 ).filter(
                     distance_km__lte=distance_threshold_km
@@ -107,26 +115,26 @@ def handle_request(request):
             except Exception as e:
                 print(f"Error occured {str(e)}")
             print([item for item in offers])
-            return JsonResponse({'message': 'return'})
-        
+            return JsonResponse({'message': 'hi'})
+
         if data.get('type') == 'fetch-rides':
-            offer = Offers.objects.get( user_id = data.get('user_id'),is_active = True, is_accepted = True)
+            offer = Offers.objects.get(user_id=data.get(
+                'user_id'), is_active=True, is_accepted=True)
             driverid = offer.driver_id
-            driver = User.objects.get(id = driverid)
+            driver = User.objects.get(id=driverid)
             if offer is None:
-                return JsonResponse({'message':'none'})
+                return JsonResponse({'message': 'none'})
             message = {
-                    'success': 'True',
-                    'name' : driver.first_name ,
-                    'phoneNo' : driver.phone_number,
-                    'license' : driver.license_number,
-                    'requestId':offer.request_id
-                }
+                'success': 'True',
+                'name': driver.first_name,
+                'phoneNo': driver.phone_number,
+                'license': driver.license_number,
+                'requestId': offer.request_id
+            }
             return JsonResponse({'message': message})
-        
+
         if data.get('type') == 'end-ride':
-            offer = Offers.objects.get( requestId = data.get('requestId'))
+            offer = Offers.objects.get(requestId=data.get('requestId'))
             offer.is_active = False
             return JsonResponse({'message': "success"})
     return JsonResponse({'message': 'Invalid request.'}, status=400)
-
